93f8d90804b25fcd24676b0852550ee6726e785b
Moatasem AlGunaid <m3t9mm@gmail.com>
2022-04-22 08:23:07 +0400

    t

---
+++ libs/libftt/ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mal-guna <mal-guna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 11:08:34 by mal-guna          #+#    #+#             */
+/*   Updated: 2021/10/04 00:43:01 by mal-guna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	i;
+	size_t	b;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	b = 0;
+	join = malloc(sizeof(*join) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	while (s1[i])
+		join[b++] = s1[i++];
+	i = 0;
+	while (s2[i])
+	{
+		join[b] = s2[i];
+		i++;
+		b++;
+	}
+	join[b] = '\0';
+	return (join);
+}
