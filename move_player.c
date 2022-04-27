9835c84f741dc827c92d8f917dbd4a159bc154c3
Moatasem AlGunaid <m3t9mm@gmail.com>
2022-04-27 06:50:15 +0400

    fixed issue #3

--- move_player.c
+++ move_player.c
@@ -23,88 +23,108 @@ void	rotate(t_data *data, int dir, int i)
 	}
 }
 
-// NORMINETTED MOVE()
-/*static void	set_pos(t_data *data, int dir, double *unit, int max_index)
+void		strafe_r(t_data *data, double unit_x, double unit_y)
 {
-	int	cordinate[2];
+	int	index_x;
+	int	index_y;
 
-	if (abs(dir) == 1)
-	{
-		cordinate[Y] = max_index & ((int) round(data->player.y
-					+ unit[Y] * 15 * SPEED * dir) / BLOCK_SIZE);
-		cordinate[X] = max_index & ((int) round(data->player.x
-					+ unit[X] * 15 * SPEED * dir) / BLOCK_SIZE);
-		if ((data->map[cordinate[Y]][cordinate[X]] - '0') % 2 != 1)
-		{
-			data->player.x += dir * unit[X] * SPEED;
-			data->player.y += dir * unit[Y] * SPEED;
-		}
-		return ;
-	}
-	cordinate[Y] = max_index & ((int) round(data->player.y
-				- unit[X] * 15 * SPEED * (dir / -2)) / BLOCK_SIZE);
-	cordinate[X] = max_index & ((int) round(data->player.x
-				- unit[Y] * 15 * SPEED * (dir / 2)) / BLOCK_SIZE);
-	if ((data->map[cordinate[Y]][cordinate[X]] - '0') % 2 != 1)
-	{
-		data->player.x -= (dir / 2) * unit[Y] * SPEED;
-		data->player.y -= (dir / -2) * unit[X] * SPEED;
-	}
-}
-
-void	move(t_data *data, int dir)
-{
-	double	unit[2];
-	int		max_index;
-
-	max_index = data->map_width * BLOCK_SIZE - 1;
-	unit[X] = (data->player.rays[WIDTH / 2].ray_x - data->player.x)
-		/ (data->player.rays[WIDTH / 2].mag);
-	unit[Y] = (data->player.rays[WIDTH / 2].ray_y - data->player.y)
-		/ (data->player.rays[WIDTH / 2].mag);
-	set_pos(data, dir, unit, max_index);
-	if (data->player.x < 0)
-		data->player.x = 0;
-	if (data->player.x >= (data->map_width) * BLOCK_SIZE)
-		data->player.x = (data->map_width - 1) * BLOCK_SIZE;
-	if (data->player.y < 0)
-		data->player.y = 0;
-	if (data->player.y >= (data->map_height) * BLOCK_SIZE)
-		data->player.y = (data->map_height - 1) * BLOCK_SIZE;
-	init_rays_mag(data);
-}*/
-
-void	move(t_data *data, int dir)
-{
-	double unit_x;
-	double unit_y;	
-	int max_index[2];
-
-	max_index[X] = data->map_width * BLOCK_SIZE - 1; // The problem of moving through walls and not being able
-	max_index[Y] = data->map_height * BLOCK_SIZE - 1; // to walk through spaces is here.	
-	unit_x = (data->player.rays[WIDTH/2].ray_x - data->player.x)/(data->player.rays[WIDTH/2].mag);
-	unit_y = (data->player.rays[WIDTH/2].ray_y - data->player.y)/(data->player.rays[WIDTH/2].mag);
-	if(dir == 1 && (data->map[((int)round(data->player.y + unit_y*3 * SPEED)/BLOCK_SIZE) & max_index[Y]][((int)round(data->player.x + unit_x*3 * SPEED)/BLOCK_SIZE ) & max_index[X]] + '0') % 2 != 1)
-	{
-			data->player.x += unit_x * SPEED;
-			data->player.y += unit_y * SPEED;
-	}
-	else if(dir == -1 && (data->map[((int)round(data->player.y - unit_y*3 * SPEED)/BLOCK_SIZE) & max_index[Y]][((int)round(data->player.x - unit_x*3 * SPEED)/BLOCK_SIZE) & max_index[X]] + '0') % 2 != 1)
-	{
-		data->player.x -= unit_x * SPEED;
-		data->player.y -= unit_y * SPEED;
-	}
-	else if(dir == 2 && (data->map[((int)round(data->player.y - -unit_x*3 * SPEED)/BLOCK_SIZE) & max_index[Y]][((int)round(data->player.x - unit_y*3 * SPEED)/BLOCK_SIZE) & max_index[X]] + '0') % 2 != 1)
+	index_y = ((int)round(data->player.y - - unit_x*15 * SPEED)/BLOCK_SIZE);
+	if(index_y > data->map_height - 1)
+		index_y = data->map_height - 1;
+	if(index_y < 0)
+		index_y = 0;
+	index_x = ((int)round(data->player.x - unit_y*15 * SPEED)/BLOCK_SIZE);
+	if(index_x > data->map_width - 1)
+		index_x = data->map_width - 1;
+	if(index_x < 0)
+		index_x = 0;
+	if((data->map[index_y][index_x] - '0') != 1 && (data->map[index_y][index_x] - '0') != 3)
 	{
 		data->player.x -= unit_y * SPEED;
 		data->player.y -= -unit_x * SPEED;
 	}
-	else if(dir == -2 && (data->map[((int)round(data->player.y - unit_x*3 * SPEED)/BLOCK_SIZE) & max_index[Y]][((int)round(data->player.x - - unit_y*3 * SPEED)/BLOCK_SIZE) & max_index[X]] + '0') % 2 != 1)
+}
+
+void		strafe_l(t_data *data, double unit_x, double unit_y)
+{
+	int	index_x;
+	int	index_y;
+
+	index_y = ((int)round(data->player.y -  unit_x*15 * SPEED)/BLOCK_SIZE);
+	if(index_y > data->map_height - 1)
+		index_y = data->map_height - 1;
+	if(index_y < 0)
+		index_y = 0;
+	index_x = ((int)round(data->player.x - - unit_y*15 * SPEED)/BLOCK_SIZE);
+	if(index_x > data->map_width - 1)
+		index_x = data->map_width - 1;
+	if(index_x < 0)
+		index_x = 0;
+	if((data->map[index_y][index_x] - '0') != 1 && (data->map[index_y][index_x] - '0') != 3)
 	{
 		data->player.x -= -unit_y * SPEED;
 		data->player.y -= unit_x * SPEED;
+	}	
+}
+
+void		mv_bw(t_data *data, double unit_x, double unit_y)
+{
+	int	index_x;
+	int	index_y;
+
+	index_y = ((int)round(data->player.y - unit_y*15 * SPEED)/BLOCK_SIZE);
+	if(index_y > data->map_height - 1)
+		index_y = data->map_height - 1;
+	if(index_y < 0)
+		index_y = 0;
+	index_x = ((int)round(data->player.x -  unit_x*15 * SPEED)/BLOCK_SIZE);
+	if(index_x > data->map_width - 1)
+		index_x = data->map_width - 1;
+	if(index_x < 0)
+		index_x = 0;
+	if((data->map[index_y][index_x] - '0') != 1 && (data->map[index_y][index_x] - '0') != 3)
+	{
+		data->player.x -= unit_x * SPEED;
+		data->player.y -= unit_y * SPEED;
 	}
-	
+}
+
+void		mv_fw(t_data *data, double unit_x, double unit_y)
+{
+	int	index_x;
+	int	index_y;
+
+	index_y = ((int)round(data->player.y + unit_y*15 * SPEED)/BLOCK_SIZE);
+	if(index_y > data->map_height - 1)
+		index_y = data->map_height - 1;
+	if(index_y < 0)
+		index_y = 0;
+	index_x = ((int)round(data->player.x +  unit_x*15 * SPEED)/BLOCK_SIZE );
+	if(index_x > data->map_width - 1)
+		index_x = data->map_width - 1;
+	if(index_x < 0)
+		index_x = 0;
+	if((data->map[index_y][index_x] - '0') != 1 && (data->map[index_y][index_x] - '0') != 3)
+	{
+		data->player.x += unit_x * SPEED;
+		data->player.y += unit_y * SPEED;
+	}
+}
+void	move(t_data *data, int dir)
+{
+	double unit_x;
+	double unit_y;	
+
+	unit_x = (data->player.rays[WIDTH/2].ray_x - data->player.x)/(data->player.rays[WIDTH/2].mag);
+	unit_y = (data->player.rays[WIDTH/2].ray_y - data->player.y)/(data->player.rays[WIDTH/2].mag);
+	if(dir == 1)
+		mv_fw(data, unit_x, unit_y);
+	else if(dir == -1)
+		mv_bw(data, unit_x, unit_y);
+	else if(dir == 2)
+		strafe_r(data, unit_x, unit_y);
+	else if(dir == -2)
+		strafe_l(data, unit_x, unit_y);
 	if(data->player.x < 0)
 		data->player.x = 0;
 	if(data->player.x >= (data->map_width) * BLOCK_SIZE)
